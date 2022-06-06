925e9036d38ee4e5350dc069578cf959288923a7
nwaamadioha <udokwu.temple960@gmail.com>
2022-06-06 18:59:44 +0100

    changed BrowserRouter to Hashr Hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router , Routes, Route, Navigate, useLocation} from "react-router-dom";
+import { HashRouter as Router , Routes, Route, Navigate, useLocation} from "react-router-dom";
 import Home from './pages';
 import LoginPage from "./pages/login";
 import DashboardPage from './pages/dashboard';
