1cec78d4d463f17ed3ba0e559026ceba070dc360
nwaamadioha <udokwu.temple960@gmail.com>
2022-06-08 10:24:01 +0100

    partner section

--- src/components/Partners/PartnerElements.js
+++ src/components/Partners/PartnerElements.js
@@ -1,20 +1,45 @@
 import styled from 'styled-components'
 
 export const PartnerContainer = styled.div`
-    background: #bec7f8;
-    height: 100px;
+    background: #fff;
+    height: 300px;
     width: 100%
 `
 
+export const PartnerContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 30px 0;
+`
 export const PartnerWrapper = styled.div`
-
+    height: 80%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-evenly;
+    margin: 10px 20px;
+    padding-top: 20px;
 `
 
 export const ImgWrap = styled.div`
-  max-width: 555px;
+  max-width: 200px;
   height: 100%;
+  padding: 5px;
 `
 
+export const PartnerH1 = styled.h1`
+
+text-align: center;
+font-size: 16px;
+color: #010606;
+margin-bottom: 64px;
+@media screen and (max-width: 768px){
+  font-size: 14px;
+}
+@media screen and (max-width: 400px){
+  font-size: 1.5rem;
+}
+`
 export const Img = styled.img`
   width: 100%;
   margin: 0 0 10px 0;
--- src/components/Partners/index.js
+++ src/components/Partners/index.js
@@ -1,16 +1,41 @@
 import React from 'react'
-import {PartnerContainer, ImageWrap, Img, PartnerWrapper} from "./PartnerElements.js"
-
+import {PartnerContainer, PartnerContent, ImgWrap, Img, PartnerH1, PartnerWrapper} from "./PartnerElements.js"
+import img1 from "../../images/nfis.jpeg"
+import img2 from "../../images/member1.jpeg"
+import img3 from "../../images/member2.jpeg"
+import img4 from "../../images/member3.jpeg"
+import img5 from "../../images/member4.jpeg"
+import img6 from "../../images/member5.jpeg"
+// import img1 from "../../images/nfis.jpeg"
 
 const PartnerSection = () => {
   return (
     <>
         <PartnerContainer>
+          <PartnerContent>
+            <PartnerH1>PARTNERS & RECOGNITIONS</PartnerH1>
             <PartnerWrapper>
-                <ImageWrap>
-                  <Img src="../../images/nfis.jpeg"/>
-                </ImageWrap>
+                
+                <ImgWrap>
+                  <Img src={img1}/>
+                </ImgWrap>
+                <ImgWrap>
+                  <Img src={img2}/>
+                </ImgWrap>
+                <ImgWrap>
+                  <Img src={img3}/>
+                </ImgWrap>
+                <ImgWrap>
+                  <Img src={img4}/>
+                </ImgWrap>
+                <ImgWrap>
+                  <Img src={img5}/>
+                </ImgWrap>
+                <ImgWrap>
+                  <Img src={img6}/>
+                </ImgWrap>
             </PartnerWrapper>
+            </PartnerContent>
         </PartnerContainer>
     </>
   )
